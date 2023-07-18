05019634da7ddcbb60060ada25f04306187cc748
Eduardo Nieto <e.nieto@trii.co>
2023-07-17 22:44:02 -0500

    update firebase

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAXnNVxnL6dD8npsXHswql4YdebZZ2LbRI",
+  authDomain: "todo-keyner.firebaseapp.com",
+  projectId: "todo-keyner",
+  storageBucket: "todo-keyner.appspot.com",
+  messagingSenderId: "601003881318",
+  appId: "1:601003881318:web:c8b4ce71596c11a1e40ea7",
+  measurementId: "G-JHK3ZYFSK4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
